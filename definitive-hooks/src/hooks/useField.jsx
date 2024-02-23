ddaac7f71c2039be831decbf3191f99b1bd9f72f
jorgnavarro <jorgluisnavarro@gmail.com>
2024-02-23 13:36:04 -0500

    comments-added

--- definitive-hooks/src/hooks/useField.jsx
+++ definitive-hooks/src/hooks/useField.jsx
@@ -19,3 +19,15 @@ export const useField = (type) => {
         setValue
     ]
 }
+
+/*
+ **En este custom hook, useField, cuando es llamando en donde se consumirá, recibirá por props el type, que a su vez
+se enviará en el objeto elementAtributes, que contiene el valor y el evento que controla los cambios en el input
+onChange. Para que sea un input controlado, el value debe estar configurado.
+
+En este caso se retorna un array con dos posiciones, la primera contiene todos los elementos que serán consumidos por 
+el input que hago uso del custom hooks. En la segunda posición setValue, se usará para que desde la función que 
+haga el submit del formulario, se haga el reset del mismo y el input quede vacío
+
+Es importante recordar que en toda función de custom hooks, siempre estarán las funciones auxiliares que generalmente afectarán los estados de nuestra aplicación, con ello descargamos responsabilidad de nuestros componentes al tiempo que reutilizamos funciones en nuestros custom hooks, lo que hace más fácil mantener nuestra aplicación.
+ */
--- definitive-hooks/src/hooks/useResource.jsx
+++ definitive-hooks/src/hooks/useResource.jsx
@@ -1,30 +1,32 @@
-import { useState, useEffect } from "react"
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export const useResource = (url) => {
-    const [data, setData] = useState([])
-   
-    
-    useEffect(() => {
-        if(data){
-            axios.get(url).then(response => {
-                setData(response.data)
-            })
-        }else{
-            console.log('Data not found')
-        }
-        
-    }, [url, data])
+  const [data, setData] = useState([]);
 
-    const create =  async (object) => {
-        const response = await axios.post(url, object)
-        data.concat(response.data)
+  useEffect(() => {
+    if (data) {
+      axios.get(url).then((response) => {
+        setData(response.data);
+      });
+    } else {
+      console.log("Data not found");
     }
+  }, [url, data]);
 
+  const create = async (object) => {
+    const response = await axios.post(url, object);
+    data.concat(response.data);
+  };
 
-    return[
-        data,
-        create
-    ]
-    
-}
+  return [data, create];
+};
+
+/*
+Con useResource, estamos creando un custom hook que se comunica con nuestro servidor. Básicamente estamos realizando dos peticiones
+La primera en donde obtenemos todos los recursos del servidor y lo almacenamos en el estado "data". 
+La segunda petición crea un nuevo recurso, luego de crearlo, lo concatenamos al estado "data", que figura como dependencia del useEffect, que trae todos los recursos, es decir que siempre que se agregue un nuevo recurso, se hará un nuevo llamado al servidor para que traiga todos los datos actualizados, cuando cambie el valor "data".
+
+Retornamos entonces, un array con dos posiciones, la primera con todos los recursos y la segunda con la función que nos permite agregar nuevos recursos.
+
+*/
