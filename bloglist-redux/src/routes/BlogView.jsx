1a7cdd5bb78d63417c16b38a4b644f69a7200a24
jorgnavarro <jorgluisnavarro@gmail.com>
2024-04-28 16:36:02 -0500

    handlers-added

--- bloglist-redux/src/routes/BlogView.jsx
+++ bloglist-redux/src/routes/BlogView.jsx
@@ -8,6 +8,47 @@ const BlogView = ({ blogView }) => {
   const [like, setLike] = useState(blogView?.likes)
   const userId = useSelector(state => state.userData)
 
+
+  const handleDeleteBlog = (id) => {
+    Swal.fire({
+        title: `Are you sure to delete ${blogView.title} from your blogs?`,
+        text: 'You won\'t be able to revert this!',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          deletedABlogMutation.mutate(id)
+        }
+      })
+}
+
+
+const handleLikes = () => {
+    const blogUpdated = {
+        ...blogView,
+        likes: like + 1
+    }
+    updateBlogMutation.mutate({...blogUpdated})
+}
+
+const returnHome = () => {
+    navigate("/")
+}
+console.log(blogView)
+//agregamos los signos de interrogación para cuando se elimine el blog no arroje ningún error por consola al estar undefined.
+
+const handleComments = (e) => {
+    e.preventDefault()
+    console.log(comment)
+    console.log(blogView.id)
+    const idBlog = blogView.id
+    addCommentMutation.mutate({comment, idBlog})
+    setComment('')
+}
+
   return(
     <div className="containerViewBlog">
       <h2>{blogView?.title}</h2>
