ae6fb1fbff9bee44c592e1f5bd45d937049136fe
jorgnavarro <jorgluisnavarro@gmail.com>
2024-05-01 12:29:36 -0500

    updates

--- bloglist-react-query/src/userRequests.js
+++ bloglist-react-query/src/userRequests.js
@@ -2,4 +2,4 @@ import userService from './services/user'
 
 
 
-export const getUsers = () => userService.getAllUsers().then(response => response)
+export const getUsers = () => userService.getAllUsers ().then(response => response)
